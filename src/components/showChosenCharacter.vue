f5fa8048fb1ade2e04eaf0222ce4866708935a12
GabrielCerveira <gabrieloliveira033@outlook.com>
2022-10-18 16:29:16 -0300

    verificação dos campos

--- src/components/showChosenCharacter.vue
+++ src/components/showChosenCharacter.vue
@@ -3,7 +3,7 @@
 
     <!-- Imagem do Pokemon-->
     <div v-if="img" class="col-2 flex flex-center">
-      <div class="circle bg-secondary text-center flex flex-center text-whitePokebola">
+      <div class="circle bg-white text-center flex flex-center text-whitePokebola">
       <img
       class="absolute"
       :src="img"
@@ -17,45 +17,50 @@
     </div>
 
     <!-- Geração -->
-    <div class="flex flex-center col-7 justify-around no-wrap ">
-      <div
-            v-bind:class="[isCorrect ? 'correctClass' : 'wrongClass'] + ' circle text-center flex flex-center text-whitePokebola column'"
-      >
-        <q-icon class="right absolute" name="mdi-arrow-up-circle"/>
+    <div class="flex flex-center relative-position col-7 justify-around no-wrap ">
+      <div v-bind:class="[isCorrect(generation, testeGen.generation) ? 'correctClass' : 'wrongClass'] + ' circle text-center flex flex-center text-whitePokebola column'">
         <div>
           {{ generation }}
         </div>
         <div>
           GEN
         </div>
+        <q-icon v-if="!isCorrect(generation, testeGen.generation)" :class="[statusCondition(generation, testeGen.generation) ? '':'rotate-180']+' absolute positionIcon'" name="mdi-arrow-up-circle" size="21px"/>
       </div>
     <!-- Primeira caracteristica -->
-      <div  class="circle bg-secondary text-center flex flex-center text-whitePokebola">{{ type1 }}</div>
+      <div  v-bind:class="[isCorrect(type1, testeGen.type1) ? 'correctClass' : 'wrongClass'] + ' circle text-center flex flex-center text-whitePokebola column'">
+        {{ type1 }}
+      </div>
     <!-- Segunda característica -->
-      <div  class="circle bg-secondary text-center flex flex-center text-whitePokebola">{{ type2 }}</div>
+      <div  v-bind:class="[isCorrect(type2, testeGen.type2) ? 'correctClass' : 'wrongClass'] + ' circle text-center flex flex-center text-whitePokebola column'">
+          {{ type2 }}
+      </div>
     <!-- Peso do Pokemon-->
-      <div  class="circle bg-secondary text-center flex flex-center text-whitePokebola column">
+      <div  v-bind:class="[isCorrect(weight, testeGen.weight) ? 'correctClass' : 'wrongClass'] + ' circle text-center flex flex-center text-whitePokebola column'">
         <div>
           {{ weight }}
         </div>
         <div>
-          M
+          KG
         </div>
+        <q-icon v-if="!isCorrect(weight, testeGen.weight)" :class="[statusCondition(weight, testeGen.weight) ? '':'rotate-180']+' absolute positionIcon'" name="mdi-arrow-up-circle" size="21px"/>
       </div>
       <!-- Altura do Pokemon-->
-      <div  class="circle bg-secondary text-center flex flex-center text-whitePokebola column">
+      <div  v-bind:class="[isCorrect(height, testeGen.height) ? 'correctClass' : 'wrongClass'] + ' circle text-center flex flex-center text-whitePokebola column'">
         <div>
           {{ height }}
         </div>
         <div>
-          KG
+          m
         </div>
+        <q-icon v-if="!isCorrect(height, testeGen.height)" :class="[statusCondition(height, testeGen.height) ? '':'rotate-180']+' absolute positionIcon'" name="mdi-arrow-up-circle" size="21px"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'showChosenCharacter',
   props: {
@@ -79,9 +84,40 @@ export default {
     },
     generation: {
       type: Number
-    },
-    isCorrect: {
-      type: Boolean
+    }
+
+  },
+  setup (props) {
+    const testeGen = {
+      id: 1,
+      generation: 1,
+      title: 'Pikachu',
+      type1: 'electric',
+      type2: 'electric',
+      height: 4,
+      weight: 60
+    }
+    const statusCondition = (inputVerify, resultVerify) => {
+      if (inputVerify > resultVerify) {
+        return false
+      } else {
+        return true
+      }
+    }
+
+    const isCorrect = (inputVerify, resultVerify) => {
+      console.log(inputVerify, resultVerify)
+      if (inputVerify === resultVerify) {
+        return true
+      } else {
+        return false
+      }
+    }
+
+    return {
+      statusCondition,
+      isCorrect,
+      testeGen
     }
   }
 }
@@ -99,16 +135,16 @@ export default {
 .correctClass{
   background-color: $positive !important
 }
-.correctClass::after {
-  background-color: red!important;
-  width: 8px;
-  height: 8px;
-  border-radius: 50%;
-  position: absolute;
-}
 
 .wrongClass{
   background-color: $negative !important
+}
+
+.positionIcon {
+margin-right: -40px;
+margin-top: -40px;
+background-color: red;
+border-radius: 50%;
 }
 
 .characterName{
